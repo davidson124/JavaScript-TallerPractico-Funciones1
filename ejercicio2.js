ec0d6d2e0cdcd79f23b7dd405ec9498a7a29757c
davidson124 <daferdinat@gmail.com>
2025-11-18 13:32:07 -0500

    Solución ejercicio 2, en consola se imprimen dos resultados

---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+const numeros = [4, 6, 80, 19, 22, 30, 44, 29, 1, 3];
+
+function NumMa(numerosArray){
+    let numeroMayor = numeros[0];
+    for(let d = 0; d <= numerosArray.length; d++){
+        if(numeroMayor < numerosArray[d] ){
+            numeroMayor = numerosArray[d];
+            console.log(`El número mayor es ${numeroMayor}`)
+        }
+    }
+}
+NumMa(numeros);
