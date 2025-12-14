9e417bb3b0dec005a0d15dcf6d82b658c8419704
davidson124 <daferdinat@gmail.com>
2025-12-14 10:58:54 -0500

    Correción jercicio 3 = ciclo for y setTimeout

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,17 +1,27 @@
-// 3. Contar elementos pares:
-// ○ Función contarPares(array) que cuente
+// 3. CONTAR ELEMENTOS PARES 
+
+// * Función contarPares(array) que cuente
 // cuántos números pares hay.
-// ○ Ejemplo: contarPares([3, 8, 2, 10]) →
+// * Ejemplo: contarPares([3, 8, 2, 10]) →
 
 const numPares =[2, 5, 6, 34, 29, 18, 20, 56, 40];
 
+
+
 function contPares (numerosParesArray){
-    let contador = 0;
-    for(let d = 0; d<=numerosParesArray.length; d++){
-        if(d == d/2){
-            contador = d += 1;
+
+    console.log('HALLAREMOS LA CANTIDAD DE PARES EN LA LISTA');
+    console.log(`La lista se compone de: ${numerosParesArray}`);
+
+    setTimeout(()=>{
+        let pares = numerosParesArray[0];
+   
+    for(let d = 1; d < numerosParesArray.length; d++){
+        if( numerosParesArray[d] % 2 === 0){
+            pares += 1;
         }
     }
-    alert(`la cantidad de números pares son ${contador}`);
+    alert(`la cantidad de números pares son ${pares}`);
+    },1800) 
 }
 contPares(numPares);
