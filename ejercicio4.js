c14ece9bb698b1ef2a75509d8f315185343c5d01
davidson124 <daferdinat@gmail.com>
2025-12-14 14:01:52 -0500

    Mejora ejercicio 4, us do-while y set time out

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,10 +1,23 @@
+// 4. INVERTIR ARRAY
+
+// * Función invertirArray(array) que retorne un nuevo array invertido.
+// * Ejemplo: invertirArray(["a", "b","c"]) → ["c", "b", "a"].
+
+
 const invert = ['david', 'pedro', 'carlos', 'ana'];
-function invertirArray(invertido){
-    console.log(invertido);
-    for(let d = invertido.length -1; d >= 0; d-- ){
-        console.log(invertido[d]);
-        invertido.push(invertido[d]);
-    }
-    return invertirArray;
+
+function invertArray (ArrayInvertido){
+
+    console.log('INVERTIREMOS LA LISTA');
+    console.log(`La lista ha invertir es: ${ArrayInvertido}`);
+setTimeout(()=>{
+let d = ArrayInvertido.length-1;
+    do{
+        console.log('*',ArrayInvertido[d])
+        d--
+    }while(d >= 0)
+        
+},1300)
 }
-invertirArray(invert);
+invertArray(invert);
+
