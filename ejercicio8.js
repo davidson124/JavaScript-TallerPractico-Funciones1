516e8c8d75221f962e70f06e3e8e70cacfa8c240
davidson124 <daferdinat@gmail.com>
2025-12-15 13:50:26 -0500

    Solución ejercicio 8, agregar propiedad a objeto

---
+++ ejercicio8.js
@@ -0,0 +1,18 @@
+// 8. AGREGAR PROPIEDAD A OBJETO
+
+// * Función agregarPropiedad(objeto, clave, valor) que añada una nueva propiedad a un objeto.
+// * Ejemplo:
+// agregarPropiedad({ nombre: "Luis" }, "edad", 25) → { nombre: "Luis", edad: 25 }
+//SOLUTION
+
+const lista =[];
+
+function agregarPropiedad(AgregarLista){
+    {   
+        lista.nombre = "David",
+        lista.edad= 26,
+        lista.cargo="Auxiliar de enfermería"
+    };
+    console.log(lista);
+}
+agregarPropiedad(lista)
