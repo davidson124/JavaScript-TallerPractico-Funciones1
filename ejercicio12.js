04325149bbdb7211c5175ede17dba858b7084e32
davidson124 <daferdinat@gmail.com>
2025-12-16 09:25:09 -0500

    Creacion del fichero: ejercicio 12, queda con encabezado para posterior solución

---
+++ ejercicio12.js
@@ -0,0 +1,5 @@
+// 12. Buscar el estudiante con la nota más alta:
+
+// * Función mejorEstudiante(listaEstudiantes) que reciba un array de objetos estudiante y devuelva el nombre del que tenga la nota más alta.
+
+//SOLUTION
